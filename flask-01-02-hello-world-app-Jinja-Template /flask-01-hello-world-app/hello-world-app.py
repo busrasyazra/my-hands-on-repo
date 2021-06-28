244ae2963bb5e5df7699004364eeafefccdcd302
busrasyazra <81545300+busrasyazra@users.noreply.github.com>
2021-06-28 16:21:11 -0700

    initial file

--- flask-01-02-hello-world-app-Jinja-Template /flask-01-hello-world-app/hello-world-app.py
+++ flask-01-02-hello-world-app-Jinja-Template /flask-01-hello-world-app/hello-world-app.py
@@ -4,19 +4,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def head():
-    return "I like Flask"
+    return "I like Flask!"
 
 @app.route('/second')
 def second():
-    return "This is the second page"  
+    return "This is the second page"
 
 @app.route('/third/subthird')
 def third():
-    return "This is the subpage of third page"    
+    return "This is the subpage of third page"
 
 @app.route('/forth/<string:id>')
 def forth(id):
-    return f'Id of this page is {id}'  
+    return f'Id of this page is {id}'
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
+    # app.run(debug=True)
